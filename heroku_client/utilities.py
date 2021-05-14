c220ff402eeca46b1a37c4c7b27d4b0c399c6511
Uchechukwu Orji <orjiuchechukwu52@yahoo.com>
2021-05-14 04:50:19 +0100

    Refactored to classes and added endpoints for delete and listing apps

---
+++ heroku_client/utilities.py
@@ -0,0 +1,40 @@
+import requests
+from heroku_clent.exceptions import HerokuException
+import re
+
+
+def handle_error(response):
+
+    try:
+        response.raise_for_status()
+    except requests.exceptions.RequestException as exc:
+        # Show error from API
+        print(response.json())
+        raise HerokuException(f"{exc.args}") from exc
+
+
+def get_commit_sha(git_url, branch="main"):
+    """
+    github_url => URL of the Github repository.
+    branch => repository branch name
+
+    Returns the recent commit SHA of the code.
+    """
+
+    headers = {"Accept": "application/vnd.github.v3+json"}
+    github_regex = re.compile(
+        r"(?:https?://)?(?:www.)?github.com/(?P<user>.*)/(?P<repo>.*)/?"
+    )
+    parser = github_regex.search(git_url)
+    if parser is not None:
+        m = parser.groupdict()
+        response = requests.get(
+            f'https://api.github.com/repos/{m["user"]}/{m["repo"]}/branches/{branch}',
+            headers=headers,
+        )
+        handle_error(response)
+        data = response.json()
+        return data["commit"]["sha"]
+    raise ValueError(
+        "Improperly configured GitHub URL. Check to see the link is correct"
+    )
