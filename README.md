83586bbfc4b78f872d0111f10c5bf4fc2eee971e
Uchechukwu Orji <orjiuchechukwu52@yahoo.com>
2021-05-14 04:50:19 +0100

    Refactored to classes and added endpoints for delete and listing apps

---
+++ README.md
@@ -0,0 +1,3 @@
+# heroku-py
+
+A Python 3 wrapper around the [v3 Heroku API](#https://devcenter.heroku.com/categories/platform-api)
--- heroku_client/heroku_client.py
+++ heroku_client/heroku_client.py
@@ -1,110 +1,108 @@
 import requests
 import re
 from heroku_client.constants import HEROKU_API_URL
-from heroku_client.exceptions import HerokuException
 from heroku_client import authorization
-
-HEROKU_API_KEY = authorization.get_api_key()
-
-headers = {
-    "Accept": "application/vnd.heroku+json; version=3",
-    "Authorization": f"Bearer {HEROKU_API_KEY}",
-}
+from heroku_clent.utilities import get_commit_sha, handle_error
 
 
-def handle_error(response):
+class HerokuClient:
+    def __init__(self, api_key=None):
+        """
+        Create an instance of a heroku client for making API requests.
+        If api_key is None, will try to load API key from .netrc file in
+        current user's home directory or load from environment variable
+        HEROKU_API_KEY.
+        """
 
-    try:
-        response.raise_for_status()
-    except requests.exceptions.RequestException as exc:
-        # Show error from API
-        print(response.json())
-        raise HerokuException(f"{exc.args}") from exc
+        if api_key is None:
+            self.HEROKU_API_KEY = authorization.get_api_key()
+        else:
+            self.HEROKU_API_KEY = api_key
 
+        self.headers = {
+            "Accept": "application/vnd.heroku+json; version=3",
+            "Authorization": f"Bearer {self.HEROKU_API_KEY}",
+        }
 
-def create_app(app_name):
-    """
-    app_name should conform to the pattern as specified in the Heroku API.
-    Returns the response from the server.
-    """
+    def create_app(self, app_name):
+        """
+        app_name should conform to the pattern as specified in the Heroku API.
+        Returns the response from the server.
+        """
 
-    name_regex = re.compile(r"^[a-z][a-z0-9-]{1,28}[a-z0-9]$")
-    if name_regex.search(app_name) is None:
-        raise ValueError(
-            "Improper name configuration. Names must begin with an alphabet, can consist of numbers and hyphens as seperators."
-        )
-    payload = {"name": app_name}
-    response = requests.post(HEROKU_API_URL, headers=headers, data=payload)
-    handle_error(response)
-    return response.json()
+        name_regex = re.compile(r"^[a-z][a-z0-9-]{1,28}[a-z0-9]$")
+        if name_regex.search(app_name) is None:
+            raise ValueError(
+                "Improper name configuration. Names must begin with an alphabet, can consist of numbers and hyphens as seperators."
+            )
+        payload = {"name": app_name}
+        response = requests.post(HEROKU_API_URL, headers=self.headers, json=payload)
+        handle_error(response)
+        return response.json()
 
-
-def get_app_info(app_name_or_id):
-    """
-    Gets the details about an app given its app_name_or_id.
-    """
-    response = requests.get(f"{HEROKU_API_URL}/{app_name_or_id}", headers=headers)
-    handle_error(response)
-    return response.json()
-
-
-def get_commit_sha(git_url, branch="main"):
-    """
-    github_url => URL of the Github repository.
-    branch => repository branch name
-
-    Returns the recent commit SHA of the code.
-    """
-
-    headers = {"Accept": "application/vnd.github.v3+json"}
-    github_regex = re.compile(
-        r"(?:https?://)?(?:www.)?github.com/(?P<user>.*)/(?P<repo>.*)/?"
-    )
-    parser = github_regex.search(git_url)
-    if parser is not None:
-        m = parser.groupdict()
+    def get_app_info(self, app_name_or_id):
+        """
+        Gets the details about an app given its app_name_or_id.
+        """
         response = requests.get(
-            f'https://api.github.com/repos/{m["user"]}/{m["repo"]}/branches/{branch}',
-            headers=headers,
+            f"{HEROKU_API_URL}/{app_name_or_id}", headers=self.headers
         )
         handle_error(response)
-        data = response.json()
-        return data["commit"]["sha"]
-    raise ValueError(
-        "Improperly configured GitHub URL. Check to see the link is correct"
-    )
+        return response.json()
 
+    def delete_app(self, app_name_or_id):
+        """
+        Deletes an app given its app_name_or_id.
+        """
+        _headers = self.headers.copy()
+        _headers["Content-Type"] = "application/json"
 
-def build_from_source(git_url, app_name_or_id, branch="main", version=None):
-    """
-    git_url => github repository url of the source code.
-    app_name_or_id => the application's name or id on Heroku. Use the `get_app_info` utility
-                        to get the application's details.
-    branch => the git branch to get the source code from. Defaults to `main`.
-    version => A piece of metadata that you use to track what version of your
-                source code originated this build. If version is not specified,
-                will use the commit hash from the git_url as the version.
-    ==========================================================================
+        response = requests.delete(
+            f"{HEROKU_API_URL}/{app_name_or_id}", headers=self._headers
+        )
+        handle_error(response)
+        return response.json()
 
-    This will cause Heroku to fetch the source tarball, unpack it and start a
-    build, just as if the source code had been pushed to Heroku using git. If
-    the build completes successfully, the resulting slug will be deployed
-    automatically to the app in a new release.
+    def list_apps(self):
+        """
+        List existing apps.
+        """
+        response = requests.get(f"{HEROKU_API_URL}", headers=self.headers)
+        handle_error(response)
+        return response.json()
 
-    (https://devcenter.heroku.com/articles/build-and-release-using-the-api#creating-builds)
-    ==========================================================================
-    """
+    def build_from_source(self, git_url, app_name_or_id, branch="main", version=None):
+        """
+        git_url => github repository url of the source code.
+        app_name_or_id => the application's name or id on Heroku. Use the `get_app_info` utility
+                            to get the application's details.
+        branch => the git branch to get the source code from. Defaults to `main`.
+        version => A piece of metadata that you use to track what version of your
+                    source code originated this build. If version is not specified,
+                    will use the commit hash from the git_url as the version.
+        ==========================================================================
 
-    payload = {"source_blob": {"url": f"{git_url}/tarball/{branch}"}}
+        This will cause Heroku to fetch the source tarball, unpack it and start a
+        build, just as if the source code had been pushed to Heroku using git. If
+        the build completes successfully, the resulting slug will be deployed
+        automatically to the app in a new release.
 
-    if version is not None:
-        payload["source_blob"]["version"] = version
-    else:
-        version = get_commit_sha(git_url, branch)
-        payload["source_blob"]["version"] = version
+        (https://devcenter.heroku.com/articles/build-and-release-using-the-api#creating-builds)
+        ==========================================================================
+        """
 
-    response = requests.post(
-        f"{HEROKU_API_URL}/{app_name_or_id}/builds", headers=headers, json=payload
-    )
-    handle_error(response)
-    return response.json()
+        payload = {"source_blob": {"url": f"{git_url}/tarball/{branch}"}}
+
+        if version is not None:
+            payload["source_blob"]["version"] = version
+        else:
+            version = get_commit_sha(git_url, branch)
+            payload["source_blob"]["version"] = version
+
+        response = requests.post(
+            f"{HEROKU_API_URL}/{app_name_or_id}/builds",
+            headers=self.headers,
+            json=payload,
+        )
+        handle_error(response)
+        return response.json()
